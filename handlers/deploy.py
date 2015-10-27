3614fc62b9b521358baf58604a4de88b7c8b8cb4
Neuron Teckid <Lene13@gmail.com>
2015-10-27 17:39:24 +0800

    rewrite backend

--- handlers/deploy.py
+++ handlers/deploy.py
@@ -4,16 +4,59 @@ import yaml
 import logging
 from flask import render_template, Blueprint, request, g
 
-from utils import send_template, json_api, post_json, parse_git_url
+from utils import json_api, post_form, parse_git_url
 from clients import gitlab, eru
 
 
 bp = Blueprint('deploy', __name__, url_prefix='/deploy')
 
 
+def _get_project(repo_url):
+    logging.debug('Get repo %s', repo_url)
+    repo = parse_git_url(repo_url)
+    logging.debug('Get project %s', repo)
+    project = gitlab.getproject(repo)
+    if not project:
+        raise ValueError('no such repository %s' % repo_url)
+    return project
+
+
+def _get_project_commits(repo_url):
+    project = _get_project(repo_url)
+    return gitlab.getrepositorycommits(int(project['id']))
+
+
 @bp.route('/')
 def deploy():
-    return render_template('deploy/app.html')
+    return render_template('deploy/index.html', projects=eru.list_apps())
+
+
+@bp.route('/projects/new')
+def projects_new():
+    return render_template('deploy/projects/new.html')
+
+
+@bp.route('/projects/detail/<project_name>')
+def project_detail(project_name):
+    return render_template('deploy/projects/detail.html',
+                           project=eru.get_app(project_name))
+
+
+@bp.route('/projects/images/<project_name>')
+def project_images_tasks(project_name):
+    return render_template('deploy/projects/images_tasks.html',
+                           tasks=eru.list_app_tasks(project_name)['tasks'],
+                           project_name=project_name)
+
+
+@bp.route('/projects/build_image/<project_name>')
+def project_build_image_entry(project_name):
+    app = eru.get_app(project_name)
+    return render_template(
+        'deploy/projects/build_image.html', project=app,
+        revisions=_get_project_commits(app['git']), pods=eru.list_pods(),
+        base_images=['ubuntu:binary-2015.09.06', 'ubuntu:python-2015.09.06',
+                     'ubuntu:pywebstd-2015.09.18'])
 
 
 @bp.route('/api/groups')
@@ -41,21 +84,14 @@ def deploy_projects():
     }]
 
 
-@bp.route('/api/projects', methods=['POST'])
+@bp.route('/api/projects/register', methods=['POST'])
 @json_api
 def register_project():
-    args = post_json()
-
+    args = post_form()
     repo_url = args['repo_url']
-    repo = parse_git_url(repo_url)
-    logging.debug('Get project %s', repo)
-
-    project = gitlab.getproject(repo)
-    if not project:
-        raise ValueError('No such repository')
-
-    logging.debug('Get app.yaml for %s:%s', project['id'], project['name_with_namespace'])
-
+    project = _get_project(repo_url)
+    logging.debug('Get app.yaml for %s:%s', project['id'],
+                  project['name_with_namespace'])
     appyaml = gitlab.getrawfile(project['id'], 'master', 'app.yaml')
     if not appyaml:
         raise ValueError('no app.yaml in repository')
@@ -63,8 +99,7 @@ def register_project():
 
     commit_id = gitlab.getrepositorycommit(project['id'], 'master')['id']
     logging.debug('Loaded app.yaml for %s / %s', appconfig['appname'], appyaml)
-
-    eru.register_app_version(commit_id, repo_url, None, appconfig)
+    eru.register_app_version(commit_id, repo_url, '', appconfig)
     return ''
 
 
@@ -79,39 +114,6 @@ def list_pods():
 def base_iamges():
     return ['ubuntu:binary-2015.09.06', 'ubuntu:python-2015.09.06',
             'ubuntu:pywebstd-2015.09.18']
-
-
-@bp.route('/api/projects/<int:project_id>')
-@json_api
-def project_detail(project_id):
-    p = gitlab.getproject(project_id)
-    if not p:
-        raise ValueError('No project named %s' % project_id)
-
-    appyaml = gitlab.getrawfile(p['id'], 'master', 'app.yaml')
-    if not appyaml:
-        raise ValueError('No app.yaml set for project %s' % project_id)
-
-    appconfig = yaml.load(appyaml)
-    build = appconfig.get('build', [])
-    if not isinstance(build, list):
-        build = [build, ]
-
-    return {
-        "id": project_id,
-        "name": p['name'],
-        "repo_url": p['ssh_url_to_repo'],
-        "base_image": "----",
-        "online_ver": "----",
-        "enable_ci": False,
-        "comment": '----',
-        "group_id": p['namespace']['id'],
-        "group": {
-            "id": p['namespace']['id'],
-            "name": p['namespace']['name'],
-        },
-        "build_script": build,
-    }
 
 
 @bp.route('/api/projects/<int:project_id>/git_refs')
@@ -155,24 +157,24 @@ def project_versions():
     return format_versions(eru.list_app_versions(appname, (page - 1) * 40, 40)['versions'])
 
 
-@bp.route('/api/project_versions', methods=['POST'])
+@bp.route('/api/projects/build_image', methods=['POST'])
 @json_api
 def project_build_image():
-    args = post_json()
-
-    project = gitlab.getproject(args['project_id'])
-    if project:
-        raise ValueError('Project not found %s' % project)
+    args = post_form()
+    logging.info('To build image project=%s revision=%s pod=%s image=%s',
+                 args['project'], args['revision'], args['pod'], args['image'])
+    app = eru.get_app(args['project'])
+    project = _get_project(app['git'])
 
     # TODO 这个 group 似乎不是这个吧...
-    group, app = project['name_with_namespace'].split('/')
+    group = project['name_with_namespace'].split('/')[0].strip()
     pod = args['pod']
-    version = args['ref_hexsha1']
-    base_image = 'docker-registry.intra.hunantv.com/nbeimage/%s' % args['base_image']
+    revision = args['revision']
+    image = 'docker-registry.intra.hunantv.com/nbeimage/%s' % args['image']
 
-    logging.info('Start building group=%s pod=%s app=%s image=%s version=%s',
-                 group, pod, app, base_image, version)
-    return eru.build_image(group, pod, app, base_image, version)
+    logging.info('Start building group=%s pod=%s app=%s image=%s rev=%s',
+                 group, pod, app['name'], image, revision)
+    return eru.build_image(group, pod, app['name'], image, revision)
 
 
 @bp.route('/api/containers', methods=['GET'])
@@ -186,8 +188,3 @@ def get_containers():
     if version:
         return eru.list_version_containers(appname, version, g.start, g.limit)
     return eru.list_app_containers(appname, g.start, g.limit)
-
-
-@bp.route('/<path:templ>')
-def deploy_templ(templ):
-    return send_template('deploy', templ)
